724c7dd7565a243bf2ef8227f6ceee8d263aeaca
Malenst <igigi650@gmail.com>
2022-09-24 10:17:39 +0300

    Project Start

---
+++ Calculator/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+    string str = "";
+    int a = 0;
+    int b = 0;
+    int res = 0;
+    str = Console.ReadLine();
+    if (str.Contains("+"))
+    {
+        a = Convert.ToInt32(str.Split("+")[0]);
+        b = Convert.ToInt32(str.Split("+")[1]);
+        res = a + b;
+    }
+    else if (str.Contains("-"))
+    {
+        a = Convert.ToInt32(str.Split("-")[0]);
+        b = Convert.ToInt32(str.Split("-")[1]);
+        res = a - b;
+    }
+    else if (str.Contains("*"))
+    {
+        a = Convert.ToInt32(str.Split("*")[0]);
+        b = Convert.ToInt32(str.Split("*")[1]);
+        res = a * b;
+    }
+    else if (str.Contains("/"))
+    {
+        a = Convert.ToInt32(str.Split("/")[0]);
+        b = Convert.ToInt32(str.Split("/")[1]);
+        res = a / b;
+    }    
+    Console.WriteLine(res);
